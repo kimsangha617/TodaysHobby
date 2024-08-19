07e1fbbf6c9f5208b4d187326ea45b55c6a6637f
kimsangha617 <kimsangha617@gmail.com>
2024-08-19 11:12:01 +0900

    로깅 레벨 디버그 설정

--- src/main/java/org/example/haas/todayshobby/controller/HomeController.java
+++ src/main/java/org/example/haas/todayshobby/controller/HomeController.java
@@ -1,9 +1,12 @@
 package org.example.haas.todayshobby.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api")
+
+@RestController
+@RequestMapping("/api")
 public class HomeController {
 
     @GetMapping("heartbeat")
--- src/main/java/org/example/haas/todayshobby/domain/Item.java
+++ src/main/java/org/example/haas/todayshobby/domain/Item.java
@@ -4,7 +4,15 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 public class Item {
 
