96a3d723371c5c2f597d503adb12345baac9c584
kimsangha617 <kimsangha617@gmail.com>
2024-11-01 18:19:08 +0900

    feat(auth): JWT 토큰 인증 코드 작성
    토큰 생성, 유효성 검증, 파싱 메소드 추가
    JWT 관련한 클래스 추가

---
+++ user/src/main/java/com/cms/security/JwtTokenProvider.java
@@ -0,0 +1,70 @@
+package com.cms.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@RequiredArgsConstructor
+@Component
+public class JwtTokenProvider {
+
+    private final Key key;
+    private final long validityInMilliseconds = 60 * 60 * 1000;
+
+    public JwtTokenProvider() {
+        this.key = Keys.secretKeyFor(SignatureAlgorithm.HS256); // 비밀 키 생성
+    }
+
+    // create JWT Token
+    public String createToken(String username, String role) {
+        Claims claims = Jwts.claims().setSubject(username);
+        claims.put("role", role);
+
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliseconds);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(key)
+                .compact();
+    }
+
+    // 토큰에서 사용자 이름 추출
+    public String getUsername(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    // 토큰에서 역할 추출
+    public String getRole(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .get("role", String.class);
+    }
+
+    // 토큰 유효성 검증
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true; // 토큰이 유효하면 true 반환
+        } catch (Exception e) {
+            return false; // 유효하지 않으면 false 반환
+        }
+    }
+
+}
