b9e6a208e2b2e43630bc864c50f0150c86429552
kimsangha617 <kimsangha617@gmail.com>
2024-11-05 17:45:06 +0900

    feignclient를 이용하여 mailgun 서비스를 사용할 계획

--- api/src/main/java/com/cms/MainApplication.java
+++ api/src/main/java/com/cms/MainApplication.java
@@ -3,10 +3,12 @@ package com.cms;
 import jakarta.persistence.EntityListeners;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.envers.repository.support.EnversRevisionRepositoryFactoryBean;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableFeignClients
 @EntityListeners(AuditingEntityListener.class)
 @EnableJpaRepositories(repositoryFactoryBeanClass = EnversRevisionRepositoryFactoryBean.class)
 @SpringBootApplication
